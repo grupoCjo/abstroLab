0e7a0f6f3184bf07e3155b12c94268c9da3f5361
grupoCjo <j.tifany@aluno.ifsp.edu.br>
2025-02-24 18:41:37 -0300

    updated dbConnection

--- backEndRepository/dbConection.js
+++ backEndRepository/dbConection.js
@@ -1,1 +1,36 @@
-   
+
+const mysql = require('mysql2');
+
+
+const sql = mysql.createPool({
+  host: '',      
+  user: '',           
+  password: '',
+  database: '', 
+  waitForConnections: true, 
+  queueLimit: 0           
+});
+
+const query = (sql, params) => {
+  return new Promise((resolve, reject) => {
+    sql.query(sql, params, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+};
+
+const closeConnection = () => {
+  sql.end(err => {
+    if (err) {
+      console.log('Erro ao fechar a conexão:', err);
+    } else {
+      console.log('Conexão com o banco de dados encerrada');
+    }
+  });
+};
+
+module.exports = { query, closeConnection };
