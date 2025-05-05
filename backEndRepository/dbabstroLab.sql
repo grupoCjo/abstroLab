8699e60d36f0b65358e17eacaf3027ebdb3f7efd
Beatriz Helena Silva <devbeatrizhelena@gmail.com>
2025-05-05 10:25:08 -0300

    updates

--- backEndRepository/dbabstroLab.sql
+++ backEndRepository/dbabstroLab.sql
@@ -1,4 +1,4 @@
-/*
+/* 
     Notas para o grupo
     REFERENCES é p FK
     ON DELETE CASCADE - Se o registro principal for apagado, apaga os registros dependentes    
@@ -7,19 +7,21 @@
 
     -Bea
 */
-CREATE DATABASE IF NOT EXISTS AbstroLab;
+
+DROP DATABASE IF EXISTS AbstroLab;
+CREATE DATABASE AbstroLab;
 USE AbstroLab;
 
 CREATE TABLE usuarios (
-    usuario_ID VARCHAR(7) PRIMARY KEY,
+    usuario_ID INT PRIMARY KEY AUTO_INCREMENT,
     usuario_nome VARCHAR(100) NOT NULL,
     usuario_data_nascimento DATE NOT NULL,
     usuario_email VARCHAR(150) UNIQUE NOT NULL
 );
 
 CREATE TABLE sessoes (
-    sessao_ID VARCHAR(10) PRIMARY KEY,
-    usuario_ID VARCHAR(7) NOT NULL,
+    sessao_ID INT PRIMARY KEY AUTO_INCREMENT,
+    usuario_ID INT NOT NULL,
     sessao_data_hora_inicio DATETIME NOT NULL,
     sessao_data_hora_fim DATETIME NOT NULL,
     sessao_duracao TIME NOT NULL,
@@ -28,14 +30,15 @@ CREATE TABLE sessoes (
 );
 
 CREATE TABLE configuracoes_usuario (
-    config_ID VARCHAR(10) PRIMARY KEY,
-    usuario_ID VARCHAR(7) NOT NULL,
+    config_ID INT PRIMARY KEY AUTO_INCREMENT,
+    usuario_ID INT NOT NULL,
     tema VARCHAR(20) NOT NULL,
     FOREIGN KEY (usuario_ID) REFERENCES usuarios(usuario_ID) ON DELETE CASCADE
 );
 
 CREATE TABLE exercicios (
-    exercicio_ID VARCHAR(10) PRIMARY KEY,
+    exercicio_ID INT PRIMARY KEY AUTO_INCREMENT,
+    exercicio_codigo VARCHAR(10) UNIQUE,
     posicao_trilha INT NOT NULL,
     titulo VARCHAR(255) NOT NULL,
     enunciado TEXT,
@@ -44,11 +47,41 @@ CREATE TABLE exercicios (
     nivel ENUM('iniciante', 'intermediario', 'avancado') DEFAULT 'iniciante',
     codigo LONGTEXT
 );
+DELIMITER //
+
+CREATE TRIGGER before_insert_exercicios
+BEFORE INSERT ON exercicios
+FOR EACH ROW
+
+BEGIN
+    DECLARE proximo_id INT;
+
+    -- Pega o próximo valor de AUTO_INCREMENT da tabela
+    SELECT AUTO_INCREMENT INTO proximo_id
+    FROM information_schema.tables
+    WHERE table_name = 'exercicios'
+      AND table_schema = DATABASE();
+
+    -- Gera o código no formato EX001, EX002, etc.
+    SET NEW.exercicio_codigo = CONCAT('EX', LPAD(proximo_id, 3, '0'));
+END;
+//
+
+DELIMITER ;
 
 CREATE TABLE progresso_exercicios (
-    usuario_ID VARCHAR(7) NOT NULL,
-    exercicio_ID VARCHAR(10) NOT NULL,
+    usuario_ID INT NOT NULL,
+    exercicio_ID INT NOT NULL,
     PRIMARY KEY (usuario_ID, exercicio_ID),
     FOREIGN KEY (usuario_ID) REFERENCES usuarios(usuario_ID) ON DELETE CASCADE,
     FOREIGN KEY (exercicio_ID) REFERENCES exercicios(exercicio_ID) ON DELETE CASCADE
 );
+USE Abstrolab;
+select * FROM exercicios;
+-- 1. Apaga todos os dados da tabela
+DELETE FROM exercicios;
+
+-- 2. Reinicia o contador AUTO_INCREMENT
+ALTER TABLE exercicios AUTO_INCREMENT = 1;
+
+TRUNCATE TABLE exercicios;
