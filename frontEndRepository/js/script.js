4026817b2bd5b3799237111c43485682988788f6
Tifany Moreira <tiluiza12@gmail.com>
2025-05-25 21:01:37 -0300

    update index.html, style.css e script.js

--- frontEndRepository/js/script.js
+++ frontEndRepository/js/script.js
@@ -25,18 +25,22 @@ document.getElementById('hamburger').addEventListener('click', function() {
     });
 });
         
-// Carrossel! ! ! 
-let index = 0;
+// Carrossel! ! !
+document.addEventListener("DOMContentLoaded", () => {
+    let index = 0;
 
-function moveCarousel(step) {
-    const carouselItems = document.querySelector('.carousel-items');
-    const total = document.querySelectorAll('.carousel-items .feature-card').length;
-    index = (index + step + total) % total;
-    carouselItems.style.transform = `translateX(-${index * 100}%)`;
-}
+    function moveCarousel(step) {
+        const carouselItems = document.querySelector('.carousel-items');
+        const total = document.querySelectorAll('.carousel-items .feature-card').length;
+        index = (index + step + total) % total;
+        carouselItems.style.transform = `translateX(-${index * 100}%)`;
+    }
 
-document.querySelector('.carousel-prev').addEventListener('click', () => moveCarousel(-1));
-document.querySelector('.carousel-next').addEventListener('click', () => moveCarousel(1));
+    document.querySelector('.carousel-prev').addEventListener('click', () => moveCarousel(-1));
+    document.querySelector('.carousel-next').addEventListener('click', () => moveCarousel(1));
+});
+
+
 
 //enviar o cadastro p DB
 
@@ -137,4 +141,6 @@ document.getElementById("entrarBtn").addEventListener("click", async () => {
   }
 });
 
-
+document.getElementById("begin").addEventListener("click", () => {
+    window.location.href = "cadastro.html";
+})
