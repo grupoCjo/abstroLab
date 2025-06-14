2e4f2214325572c5e2a8a527e52261b2ef328cb1
tifanymoreira <tiluiza12@gmail.com>
2025-06-14 20:32:27 -0300

    add log

--- server.js
+++ server.js
@@ -526,7 +526,8 @@ app.post("/login", async (req, res) => {
 
   try {
     const db = require("./backEndRepository/dbConnection")
-    const [usuarios] = await db.query("SELECT * FROM usuarios WHERE usuario_email = ?", [email]);
+    const [usuarios] = await db.query("SELECT email, FROM usuarios WHERE usuario_email = ?", [email]);
+    console.log("USUARIOS:: " + JSON.stringify(usuarios));
     const usuario = usuarios[0];
     console.log("USER EMAIL:: " + usuario.usuario_email);
   
