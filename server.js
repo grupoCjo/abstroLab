9428dec203d98c5031d2a1a39184b95908dbe019
tifanymoreira <tiluiza12@gmail.com>
2025-06-14 20:28:06 -0300

    arrumado o erro do email

--- server.js
+++ server.js
@@ -529,7 +529,7 @@ app.post("/login", async (req, res) => {
     const [usuarios] = await db.query("SELECT * FROM usuarios WHERE usuario_email = ?", [email]);
     const usuario = usuarios[0];
     
-    if (!usuario) {
+    if (!usuario_email) {
       return res.status(401).json({ message: "Email inválido" });
     }
 
